b5eac30fb9853eb38a74d37478f6c5c732cd32eb
mnm-54 <shahriarmun54@gmail.com>
2022-04-27 12:40:00 +0600

    fix(functions.php):error in function call fixed

--- functions.php
+++ functions.php
@@ -13,6 +13,8 @@ function philosophy_theme_setup()
     add_theme_support('html5', array('search-form', 'comment-list'));
     add_theme_support('post-formats', array('image', 'gallery', 'quote', 'audio', 'video', 'link'));
     add_editor_style('/assets/css/editor-style.css');
+
+    register_nav_menu('topmenu', __('Top menu', 'philosophy'));
 }
 add_action('after_setup_theme', 'philosophy_theme_setup');
 
@@ -51,30 +53,30 @@ function philosophy_assets()
     wp_enqueue_style('philosophy-css', get_stylesheet_uri(), null, VERSION);
 
     // js files
-    wp_enqueue_style(
-        'philosophy-modernizr-js',
-        get_theme_file_uri('/assets/js/modernizr.js'),
+    wp_enqueue_script(
+        "philosophy-modernizr-js",
+        get_theme_file_uri("/assets/js/modernizr.js"),
         null,
-        '1.0'
+        "1.0"
     );
-    wp_enqueue_style(
-        'philosophy-pace-js',
-        get_theme_file_uri('/assets/js/pace.min.js'),
+    wp_enqueue_script(
+        "philosophy-pace-js",
+        get_theme_file_uri("/assets/js/pace.min.js"),
         null,
-        '1.0'
+        "1.0"
     );
-    wp_enqueue_style(
-        'philosophy-plugins-js',
-        get_theme_file_uri('/assets/js/plugins.js'),
-        array('jquery'),
-        '1.0',
+    wp_enqueue_script(
+        "philosophy-plugins-js",
+        get_theme_file_uri("/assets/js/plugins.js"),
+        array("jquery"),
+        "1.0",
         true
     );
-    wp_enqueue_style(
-        'philosophy-main-js',
-        get_theme_file_uri('/assets/js/main.js'),
-        array('jquery'),
-        '1.0',
+    wp_enqueue_script(
+        "philosophy-main-js",
+        get_theme_file_uri("/assets/js/main.js"),
+        array("jquery"),
+        "1.0",
         true
     );
 }
